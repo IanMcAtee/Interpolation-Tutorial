f971bef1a38955133dcb4c57536a4fe3a7e46544
Ian McAtee <ianmcatee@protonmail.com>
2024-11-24 08:03:32 -0700

    Updated interp.py

--- interp.py
+++ interp.py
@@ -13,11 +13,8 @@ import math
 import numpy as np
 
 ##### NEAREST NEIGHBOR INTERPOLATION METHODS #####
-def foobar():
-  pass
 
-
-def nearest_neighbor(signal: np.ndarray, newLen: tuple) -> np.ndarray:
+def nearest_neighbor(signal: np.ndarray, newLen: int) -> np.ndarray:
   interval = (len(signal)-1)/(newLen-1)
   interpSignal = np.zeros(newLen)
   for i in range(newLen):
@@ -35,5 +32,22 @@ def nearest_neighbor2(img: np.ndarray, newShape: tuple) -> np.ndarray:
 
 ##### LINEAR INTERPOLATION METHODS #####
 
+def linear(signal: np.ndarray, newLen: int) -> np.ndarray:
+  interval = (len(signal)-1)/(newLen-1)
+  interpSignal = np.zeros(newLen)
+  for i in range(newLen):
+    x = i*interval
+    x0 = np.floor(x)
+    x1 = np.ceil(x)
+    if x0 == x1:
+      interpSignal[i] = signal[int(x0)]
+      continue
+    y0 = signal[int(x0)]
+    y1 = signal[int(x1)]
+    interpSignal[i] = y0+(x-x0)*((y1-y0)/(x1-x0))
+  return interpSignal
+
+def bilinear():
+  pass
 
 ##### CUBIC INTERPOLATION METHODS #####
